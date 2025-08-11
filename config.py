3300dc9a2a85c24e07033e1ba3c3698023e8724b
ali talbi <alitalbi73@gmail.com>
2025-08-12 01:37:26 +0200

    changing local_rep_path for assets referential

--- config.py
+++ config.py
@@ -3,10 +3,9 @@ import os
 import dotenv
 
 dotenv.load_dotenv()
-cwd = os.getcwd()
 repo_name = "alitalbi/storage_data_fy"
 local_repo_path = "D:\storage_data_fy\Datas"
 token = os.getenv("GIT_TOKEN")
 branch = "master"
 yahoo_interval = "1d"
-xml_file_path = os.path.join(cwd,"assets.xml")
+xml_file_path = os.path.join(local_repo_path,"assets.xml")
