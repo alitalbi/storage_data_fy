770a0801b518dc16173fe5885c824e48705c3741
ali-talbi <alitalbi73@gmail.com>
2025-08-17 17:16:26 +0200

    updating referential xml

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import dotenv
 
 dotenv.load_dotenv()
 repo_name = "alitalbi/storage_data_fy"
-local_repo_path = "D:\storage_data_fy\Datas"
+local_repo_path = os.getcwd()
 token = os.getenv("GIT_TOKEN")
 branch = "master"
 yahoo_interval = "1d"
